adb6639e548180a1b859b21758fdcb8bd4099984
Eyob Berhe <eyob@ymail.com>
2023-02-25 00:05:13 +0000

    Modify files for the notification

--- backend-flask/services/notifications_activities.py
+++ backend-flask/services/notifications_activities.py
@@ -4,8 +4,8 @@ class NotificationsActivities:
     now = datetime.now(timezone.utc).astimezone()
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-      'handle':  'Andrew Brown',
-      'message': 'Cloud is fun!',
+      'handle':  'Today is Friday',
+      'message': 'Outside is a buitiful wether and it is good to fun!',
       'created_at': (now - timedelta(days=2)).isoformat(),
       'expires_at': (now + timedelta(days=5)).isoformat(),
       'likes_count': 5,
