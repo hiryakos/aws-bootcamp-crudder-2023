e9a16df9defcbce0302b956876e5704a74b67daa
Eyob Berhe <eyob@ymail.com>
2023-03-04 23:51:23 +0000

    Modify cognito pool configuration

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -15,7 +15,7 @@ import {
   createBrowserRouter,
   RouterProvider
 } from "react-router-dom";
-/*
+
 import { Amplify } from 'aws-amplify';
 
 Amplify.configure({
@@ -32,7 +32,7 @@ Amplify.configure({
     userPoolWebClientId: process.env.REACT_APP_AWS_USER_POOLS_WEB_CLIENT_ID,   // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
   }
 });
-*/
+
 
 const router = createBrowserRouter([
   {
