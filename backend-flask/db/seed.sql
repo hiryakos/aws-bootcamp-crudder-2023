fa4c01b8820a82a4efb47044bbde96b0ab58c2ce
Eyob Berhe <eyob@ymail.com>
2023-03-31 05:05:55 +0000

    Modify seed sql

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,9 +1,9 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Eyob Berhe','eyob@ymail.com' , 'eyobberhe' ,'0f5b4b90-26d9-42c0-9de3-37b0b8d324c1'),
-  ('Eyob Tesfay','eberhe800@gmail.com' , 'tesfay' ,'18c3e3e7-43e9-4a66-9c47-1b634b4fd63c'),
-  ('Hiryakos Tesfay', 'hiryakos@gmail.com','hiryakos','b07179ba-f677-417c-a9bd-3191c3962059');
+  ('Eyob Berhe','eyob@ymail.com' , 'eyob' ,'MOCK'),
+  ('Eyob Tesfay','eberhe800@gmail.com' , 'EYOB' ,'MOCK'),
+  ('Hiryakos Tesfay', 'hiryakos@gmail.com','hiryakos','MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
